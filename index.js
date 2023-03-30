ea153be60729dc2f4558b43de6bade3f16f53205
Hugo GB <hugogaben8.02@gmail.com>
2023-03-30 20:38:27 +0200

    added endpoints for teams and drivers by team

--- index.js
+++ index.js
@@ -2,29 +2,28 @@ console.clear();
 
 import express from "express";
 
+import { teams } from "./assets/teams.js";
+import { drivers } from "./assets/drivers.js";
+
 const PORT = 3000;
 const expressApp = express();
 
 // middleware
 expressApp.use(express.json());
-expressApp.use(express.text());
 
 expressApp.get("/", (req, res) => {
 	res.send("Welcome to F1 API !!!");
+	res.end();
 });
 
-expressApp.get("/account/:id_account", (req, res) => {
-	res.send(`Account id: ${req.params.id_account}`);
+expressApp.get("/teams", (req, res) => {
+	res.send(teams);
+	res.end();
 });
 
-expressApp.get("/test-not-authorized", (req, res) => {
-	res.status(401).send({
-		errorMessage: "Not authorized",
-	});
-});
-
-expressApp.post("/my-account", (req, res) => {
-	res.send(req.body);
+expressApp.get("/drivers/:team_name", (req, res) => {
+	res.send(drivers[req.params.team_name]);
+	res.end();
 });
 
 expressApp.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
