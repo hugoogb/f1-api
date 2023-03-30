d0e8126f6e5f5598bfa5d7a264ac1d78007fcfe4
Hugo GB <hugogaben8.02@gmail.com>
2023-03-30 22:06:41 +0200

    add static page containing info

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ console.clear();
 import express from "express";
 import cors from "cors";
 
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 import { teams } from "./assets/teams.js";
 import { drivers } from "./assets/drivers.js";
 
@@ -14,16 +21,13 @@ let corsOptions = {
 	origin: ["http://localhost:5173"],
 };
 
+expressApp.use(express.static("public"));
+
 // enabling CORS for any unknown origin
 expressApp.use(cors(corsOptions));
 
 // middleware
 expressApp.use(express.json());
-
-expressApp.get("/", (req, res) => {
-	res.send("Welcome to F1 API !!!");
-	res.end();
-});
 
 expressApp.get("/teams", (req, res) => {
 	res.send(teams);
