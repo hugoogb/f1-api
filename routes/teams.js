828789c6e0ced89a6bc1977caa19fabbd4e9c8c9
Hugo GB <hugogaben8.02@gmail.com>
2023-04-04 18:21:52 +0200

    get stats of a single team

--- routes/teams.js
+++ routes/teams.js
@@ -94,4 +94,69 @@ teamsRouter.get("/", (req, res) => {
 		});
 });
 
+teamsRouter.get("/:team_name", (req, res) => {
+	const teamName = req.params.team_name.split("-");
+
+	const normalizedTeamName = teamName
+		.map((word) => {
+			return word[0].toUpperCase() + word.substring(1);
+		})
+		.join("-");
+
+	// TODO : handle incorrect names
+	const url = `https://www.formula1.com/en/teams/${normalizedTeamName}.html`;
+
+	fetch(url)
+		.then((response) => response.text())
+		.then((text) => {
+			const $ = cheerio.load(text);
+
+			let team = {};
+
+			team["name"] = $("main.template-team-details").attr("pagename");
+
+			const teamHeader = $(
+				"main.template-team-details header.team-details"
+			);
+
+			const statsSection = $(teamHeader)
+				.children("section.stats")
+				.children("div.team-stats");
+
+			team["logo"] = $(statsSection)
+				.children("section.extra-info")
+				.children("div.brand-logo")
+				.children("img")
+				.attr("src");
+
+			const statsTable = $(statsSection)
+				.children("table.stat-list")
+				.children("tbody")
+				.children("tr");
+
+			$(statsTable).each((i, stat) => {
+				const statKey = $(stat)
+					.children("th.stat-key")
+					.children("span.text")
+					.text()
+					.split(" ")
+					.join("-")
+					.toLowerCase();
+
+				const statValue = $(stat).children("td.stat-value").text();
+
+				team[statKey] = statValue;
+			});
+
+			res.send(team);
+			res.end();
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(404).send({
+				error_message: "Used parameter :team_name does not exist",
+			});
+		});
+});
+
 export default teamsRouter;
