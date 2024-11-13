7d41508cec5356e644bf400d58650f49a046b25b
Hugo GB <hugogaben8.02@gmail.com>
2024-11-13 14:41:29 +0100

    return sorted data, save to DB if data has been updated

--- api/routes/drivers.ts
+++ api/routes/drivers.ts
@@ -1,10 +1,10 @@
-import express from "express";
+import { Router } from "express";
 import { Driver } from "../types/drivers.types";
 import { PostgrestError } from "@supabase/supabase-js";
 import { scrapeDrivers } from "../scraper";
 import { supabase } from "../services/supabase";
 
-export const driversRouter = express.Router();
+export const driversRouter = Router();
 
 driversRouter.get("/", async (req, res) => {
   try {
@@ -13,18 +13,46 @@ driversRouter.get("/", async (req, res) => {
       error,
     }: { data: Driver[] | null; error: PostgrestError | null } = await supabase
       .from("drivers")
-      .select("*");
+      .select("*")
+      .order("lastName", { ascending: true });
 
-    if (error || !drivers || drivers.length === 0) {
-      const scrapedDrivers = await scrapeDrivers();
+    if (error) throw error;
+
+    const ids: number[] = drivers?.map((driver) => driver.id) || [];
+
+    const scrapedDrivers = await scrapeDrivers();
+    scrapedDrivers.sort((a, b) => a.lastName.localeCompare(b.lastName));
+
+    const isDifferent =
+      !drivers ||
+      drivers.length !== scrapedDrivers.length ||
+      drivers.some((driver) => {
+        const scrapedDriver = scrapedDrivers.find(
+          (d) =>
+            d.firstName === driver.firstName && d.lastName === driver.lastName
+        );
+        return (
+          !scrapedDriver ||
+          driver.rank !== scrapedDriver.rank ||
+          driver.points !== scrapedDriver.points ||
+          driver.team !== scrapedDriver.team ||
+          driver.countryFlag !== scrapedDriver.countryFlag ||
+          driver.image !== scrapedDriver.image ||
+          driver.numberLogo !== scrapedDriver.numberLogo
+        );
+      });
+
+    if (isDifferent) {
+      const { error: deleteError } = await supabase
+        .from("drivers")
+        .delete()
+        .in("id", ids);
+      if (deleteError) throw new Error("Error deleting old drivers");
 
       const { error: insertError } = await supabase
         .from("drivers")
         .insert(scrapedDrivers);
-
-      if (insertError) {
-        throw new Error("Error saving drivers to database");
-      }
+      if (insertError) throw new Error("Error saving new drivers to database");
 
       res.status(200).json(scrapedDrivers);
     } else {
--- api/routes/teams.ts
+++ api/routes/teams.ts
@@ -13,18 +13,44 @@ teamsRouter.get("/", async (req, res) => {
       error,
     }: { data: Team[] | null; error: PostgrestError | null } = await supabase
       .from("teams")
-      .select("*");
+      .select("*")
+      .order("name", { ascending: true });
 
-    if (error || !teams || teams.length === 0) {
-      const scrapedTeams = await scrapeTeams();
+    if (error) throw error;
+
+    const ids: number[] = teams?.map((driver) => driver.id) || [];
+
+    const scrapedTeams = await scrapeTeams();
+    scrapedTeams.sort((a, b) => a.name.localeCompare(b.name));
+
+    const isDifferent =
+      !teams ||
+      teams.length !== scrapedTeams.length ||
+      teams.some((team) => {
+        const scrapedTeam = scrapedTeams.find((t) => t.name === team.name);
+        return (
+          !scrapedTeam ||
+          team.name !== scrapedTeam.name ||
+          team.logo !== scrapedTeam.logo ||
+          team.logoSmall !== scrapedTeam.logoSmall ||
+          team.imageCar !== scrapedTeam.imageCar ||
+          JSON.stringify(team.drivers) !==
+            JSON.stringify(scrapedTeam.drivers) ||
+          team.color !== scrapedTeam.color
+        );
+      });
+
+    if (isDifferent) {
+      const { error: deleteError } = await supabase
+        .from("teams")
+        .delete()
+        .in("id", ids);
+      if (deleteError) throw new Error("Error deleting old teams");
 
       const { error: insertError } = await supabase
         .from("teams")
         .insert(scrapedTeams);
-
-      if (insertError) {
-        throw new Error("Error saving teams to database");
-      }
+      if (insertError) throw new Error("Error saving new teams to database");
 
       res.status(200).json(scrapedTeams);
     } else {
